5a9627392ab85c5d553ff7ef0508917ae4b8c9fa
Ted Spence <ted@spence.net>
2023-06-30 10:36:25 +1200

    Ability to generate new project files

--- SdkGenerator/SdkGenerator/Program.cs
+++ SdkGenerator/SdkGenerator/Program.cs
@@ -11,8 +11,11 @@ public static class Program
 {
     private class Options
     {
-        [Option('p', "Project", Required = true, HelpText = "Specify a project file")]
+        [Option('p', "Project", HelpText = "Specify a project file")]
         public string ProjectFile { get; set; }
+        
+        [Option("new", HelpText = "Create a new project file")]
+        public string CreateNewProjectFile { get; set; }
     }
 
     public static async Task Main(string[] args)
@@ -20,50 +23,83 @@ public static class Program
         await Parser.Default.ParseArguments<Options>(args)
             .WithParsedAsync(async o =>
             {
-                // Retrieve project
-                if (!File.Exists(o.ProjectFile))
+                // Did they want to create a new project file?
+                if (o.CreateNewProjectFile != null)
                 {
-                    Console.WriteLine($"Project file could not be found: {o.ProjectFile}");
-                    return;
-                }
+                    await CreateNewProject(o.CreateNewProjectFile);
+                } else 
 
-                var text = await File.ReadAllTextAsync(o.ProjectFile);
-                var project = JsonConvert.DeserializeObject<ProjectSchema>(text);
-
-                if (project is null)
+                if (o.ProjectFile != null)
                 {
-                    Console.WriteLine("Could not parse project file");
-                    return;
+                    await GenerateProject(o.ProjectFile);
                 }
-
-                // Fetch the environment and version number
-                Console.WriteLine($"Retrieving swagger file from {project.SwaggerUrl}");
-                var api = await DownloadFile.GenerateApi(project);
-                if (api == null)
+                else
                 {
-                    Console.WriteLine("Unable to retrieve API and version number successfully.");
-                    return;
+                    Console.WriteLine("Please specify either --new, -p [file], or --help.");
                 }
-
-                Console.WriteLine($"Retrieved swagger file. Version: {api.Semver2}");
-
-                // Let's do some software development kits!
-                await TypescriptSdk.Export(project, api);
-                await CSharpSdk.Export(project, api);
-                await JavaSdk.Export(project, api);
-                await RubySdk.Export(project, api);
-                await PythonSdk.Export(project, api);
-                Console.WriteLine("Exported SDKs.");
-
-                // Do we want to upload data models to readme?
-                if (project.Readme != null)
-                {
-                    Console.WriteLine("Uploading data models to Readme...");
-                    await ReadmeUpload.UploadSchemas(api, project.Readme.ApiKey, "list");
-                    Console.WriteLine("Uploaded data models to Readme.");
-                }
-
-                Console.WriteLine("Done!");
             });
+    }
+
+    private static async Task GenerateProject(string filename)
+    {
+
+        // Retrieve project
+        if (!File.Exists(filename))
+        {
+            Console.WriteLine($"Project file could not be found: {filename}");
+            return;
+        }
+
+        var text = await File.ReadAllTextAsync(filename);
+        var project = JsonConvert.DeserializeObject<ProjectSchema>(text);
+
+        if (project is null)
+        {
+            Console.WriteLine("Could not parse project file");
+            return;
+        }
+
+        // Fetch the environment and version number
+        Console.WriteLine($"Retrieving swagger file from {project.SwaggerUrl}");
+        var api = await DownloadFile.GenerateApi(project);
+        if (api == null)
+        {
+            Console.WriteLine("Unable to retrieve API and version number successfully.");
+            return;
+        }
+
+        Console.WriteLine($"Retrieved swagger file. Version: {api.Semver2}");
+
+        // Let's do some software development kits!
+        await TypescriptSdk.Export(project, api);
+        await CSharpSdk.Export(project, api);
+        await JavaSdk.Export(project, api);
+        await RubySdk.Export(project, api);
+        await PythonSdk.Export(project, api);
+        Console.WriteLine("Exported SDKs.");
+
+        // Do we want to upload data models to readme?
+        if (project.Readme != null)
+        {
+            Console.WriteLine("Uploading data models to Readme...");
+            await ReadmeUpload.UploadSchemas(api, project.Readme.ApiKey, "list");
+            Console.WriteLine("Uploaded data models to Readme.");
+        }
+
+        Console.WriteLine("Done!");
+    }
+
+    private static async Task CreateNewProject(string name)
+    {
+        var newSchema = new ProjectSchema();
+        newSchema.Environments = new[] { new EnvironmentSchema() };
+        newSchema.Readme = new ReadmeSiteSchema();
+        newSchema.Csharp = new LanguageSchema();
+        newSchema.Java = new LanguageSchema();
+        newSchema.Python = new LanguageSchema();
+        newSchema.Ruby = new LanguageSchema();
+        newSchema.Typescript = new LanguageSchema();
+        await File.WriteAllTextAsync(name + ".json", JsonConvert.SerializeObject(newSchema, Formatting.Indented));
+        Console.WriteLine($"Created new project file {name}.json");
     }
 }
